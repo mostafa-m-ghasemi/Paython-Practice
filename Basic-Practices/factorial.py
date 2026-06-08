4a5fc39c3327da18431fcd3bf9e5875b6effcaf7
Mostafa <mostafa.m.qasemi@gmail.com>
2026-06-08 08:03:04 +0330

    Update factorial.py

--- Basic-Practices/factorial.py
+++ Basic-Practices/factorial.py
@@ -1,8 +1,14 @@
-num = int(input("Enter a number: "))
-number = num
-factorial = 1
-while num > 0:
-    factorial *= num
-    num -= 1
-
-print(f" {number}! = {factorial}")
+while True:
+    try:
+        num = int(input("Enter a number: "))
+        number = num
+        factorial = 1
+        while num > 0:
+            factorial *= num
+            num -= 1
+        print(f" {number}! = {factorial}")
+    except ValueError:
+        print("Invalid input")
+        continue
+    except EOFError:
+        break
